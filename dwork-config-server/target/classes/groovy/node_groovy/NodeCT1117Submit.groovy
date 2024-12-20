6a1c91c9b5ea8e1a61c558e342cdf31841e95dcb
Ryan.Zhang <Ryan.Zhang@tengnet.com>
2024-12-20 10:57:19 +0800

    ryan submit

---
+++ dwork-config-server/target/classes/groovy/node_groovy/NodeCT1117Submit.groovy
@@ -0,0 +1,32 @@
+package groovy.node_groovy
+
+import com.alibaba.fastjson.JSONObject
+import com.chinajay.virgo.bmf.obj.BmfObject
+import com.chinajay.virgo.utils.SpringUtils
+import com.chinajey.application.common.exception.BusinessException
+import com.tengnat.dwork.modules.script.abstracts.NodeScanGroovyClass
+import com.tengnat.dwork.modules.script.service.BasicGroovyService
+
+/**
+ * 出入库工作台绑定人员
+ */
+class NodeCT1117Submit extends NodeScanGroovyClass{
+    def basicGroovyService= SpringUtils.getBean(BasicGroovyService.class)
+    @Override
+    JSONObject runScript(BmfObject nodeData) {
+        def userCode = nodeData.getString("receiveObjectCode")
+        def userName = nodeData.getString("receiveObjectName")
+        def workbenchCode = nodeData.getString("inventoryWorkbenchCode")
+
+        def workbench = basicGroovyService.findOne("inventoryWorkbench", "code", workbenchCode)
+        if (workbench == null){
+            throw new BusinessException("出入库工作台主数据不存在:" + workbenchCode)
+        }
+        workbench.put("userCode",userCode)
+        workbench.put("userName",userName)
+        basicGroovyService.updateByPrimaryKeySelective(workbench)
+        //throw new BusinessException("测试回滚......")
+        return null
+
+    }
+}
