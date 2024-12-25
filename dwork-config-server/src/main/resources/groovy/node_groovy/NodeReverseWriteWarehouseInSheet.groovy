3e62fb6b4d42e3ac96e75b9d8c7edbc86cacc3c7
wh <hao.wu@chinajey.com>
2024-12-25 10:21:00 +0800

    采购入库入库反写入库申请单

--- dwork-config-server/src/main/resources/groovy/node_groovy/NodeReverseWriteWarehouseInSheet.groovy
+++ dwork-config-server/src/main/resources/groovy/node_groovy/NodeReverseWriteWarehouseInSheet.groovy
@@ -1,6 +1,10 @@
 package groovy.node_groovy
 
 import com.chinajay.virgo.bmf.obj.BmfObject
+import com.chinajay.virgo.bmf.sql.Conjunction
+import com.chinajay.virgo.bmf.sql.OperationType
+import com.chinajay.virgo.bmf.sql.Restriction
+import com.chinajay.virgo.bmf.sql.Where
 import com.chinajay.virgo.utils.SpringUtils
 import com.chinajey.application.common.exception.BusinessException
 import com.chinajey.application.common.utils.ValueUtil
@@ -20,25 +24,65 @@ class NodeReverseWriteWarehouseInSheet extends NodeGroovyClass {
     Object runScript(BmfObject nodeData) {
         //获取周转箱信息
         List<BmfObject> passBoxes = nodeData.getList("passBoxes")
-
         def businessType = nodeData.getString("ext_business_type")
-        if ("采购入库".equals(businessType)) {
-            BigDecimal sum = passBoxes.stream().map(passBox -> passBox.getBigDecimal("quantity") == null ? BigDecimal.ZERO : passBox.getBigDecimal("quantity")).reduce(BigDecimal.ZERO, BigDecimal::add)
-            BigDecimal total = sum.add(BigDecimal.ZERO)
-            def sourceOrderCode = nodeData.getString("ext_business_source_code")
-            //获取采购入库申请单
-            def warehouseInApplication = basicGroovyService.getByCode("WarehouseInApplication", sourceOrderCode)
-            if (warehouseInApplication == null) {
-                throw new BusinessException("未找到采购入库申请单" + sourceOrderCode)
+        if (passBoxes == null || passBoxes.size() == 0 || !"采购入库".equals(businessType)) {
+            return nodeData
+        }
+        for (final def passBox in passBoxes) {
+            BigDecimal sum = passBox.getBigDecimal("quantity")
+            //
+//            def sourceOrderCode = nodeData.getString("ext_business_source_code")
+//            //获取入库申请单
+//            def warehouseInApplication = basicGroovyService.getByCode("WarehouseInApplication", sourceOrderCode)
+//            if (warehouseInApplication == null) {
+//                throw new BusinessException("未找到入库申请单" + sourceOrderCode)
+//            }
+//            List<BmfObject> warehouseInSheetDetails = warehouseInApplication.getAndRefreshList("main_idAutoMapping")
+            //匹配入库任务单
+            Map<String, Object> params = new HashMap<String, Object>()
+            params.put("passBoxRealCode", passBox.getString("code"))
+            List<BmfObject> warehouseInSheetPassBoxs = basicGroovyService.find("warehouseInSheetPassBox", params)
+            if (warehouseInSheetPassBoxs == null || warehouseInSheetPassBoxs.size() == 0) {
+                throw new BusinessException("未找到入库任务单周转箱编码" + passBox.getString("passBoxCode"))
             }
-            def warehouseInSheetDetails = warehouseInApplication.getAndRefreshList("main_idAutoMapping")
+            //入库任务单子表id
+            List<Object> ids = warehouseInSheetPassBoxs.stream().map {
+                warehouseInSheetPassBox -> warehouseInSheetPassBox.getJSONObject("warehouseInSheetDetail").getLong("id")
+            }.collect(Collectors.toList())
+            //匹配出入库任务单理论上只能匹配出一个来
+            BmfObject warehouseInSheetDetail = basicGroovyService.findOne("warehouseInSheetDetail", Where.builder()
+                    .restrictions(
+                            Arrays.asList(
+                                    Restriction.builder()
+                                            .conjunction(Conjunction.AND)
+                                            .columnName("id")
+                                            .operationType(OperationType.IN)
+                                            .values(ids)
+                                            .build(),
+                                    Restriction.builder()
+                                            .conjunction(Conjunction.AND)
+                                            .columnName("wait_inbound_quantity")
+                                            .operationType(OperationType.GRANT)
+                                            .values(Collections.singletonList(BigDecimal.ZERO))
+                                            .build()
+                            )
+                    ).build())
+            if (warehouseInSheetDetail == null) {
+                throw new BusinessException("未找到未完成的入库任务单,周转箱编码" + passBox.getString("passBoxCode"))
+            }
+            //获取入库申请单
+            def warehouseInApplication = basicGroovyService.getByCode("WarehouseInApplication",  warehouseInSheetDetail.getString("warehouseInApplicationCode"))
+            if (warehouseInApplication == null) {
+                throw new BusinessException("未找到入库申请单" +  warehouseInSheetDetail.getString("warehouseInApplicationCode"))
+            }
+            List<BmfObject> warehouseInSheetDetails = warehouseInApplication.getAndRefreshList("main_idAutoMapping")
+            //匹配瑞哭申请单子表
             //获取待修改数量
             List<BmfObject> receiptdetails = warehouseInSheetDetails.stream()
                     .filter(purchaseReceiptDetail -> {
                         BigDecimal noWarehousedQuantity = ValueUtil.toBigDecimal(purchaseReceiptDetail.getBigDecimal("wait_inbound_quantity"), BigDecimal.ZERO)
                         return noWarehousedQuantity > 0
-                    })
-                    .sorted(Comparator.comparing(purchaseReceiptDetail -> ValueUtil.toLong(purchaseReceiptDetail.getSting("lineNum"))))
+                    }).sorted(Comparator.comparing(t -> ValueUtil.toLong(((BmfObject) t).getString("lineNum"))))
                     .collect(Collectors.toList())
             //记录行号对应数量
             Map<String, BigDecimal> lineNumUpdateMap = new HashMap<>()
