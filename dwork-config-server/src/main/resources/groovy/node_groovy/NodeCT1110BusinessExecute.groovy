29f247bf4d4d16be213d0155e833ff0bd1b51737
yyj <yingjie.yuan@inc.chinajey.com>
2024-12-31 15:55:51 +0800

    单位赋值

--- dwork-config-server/src/main/resources/groovy/node_groovy/NodeCT1110BusinessExecute.groovy
+++ dwork-config-server/src/main/resources/groovy/node_groovy/NodeCT1110BusinessExecute.groovy
@@ -59,20 +59,19 @@ class NodeCT1110BusinessExecute extends  NodeGroovyClass {
             BmfObject objTask = new BmfObject("CT1118Tasks")
             objTask.put("materialCode", passBox.getString("materialCode"))//物料编码
             objTask.put("materialName", passBox.getString("materialName"))//物料描述
-            //TODO
-            // objTask.put("quantityUnit", passBox.getAndRefreshBmfObject("quantityUnit"))//计量单位
+            objTask.put("quantityUnit",  basicGroovyService.getByCode ("material",passBox.getString("materialCode")).getAndRefreshBmfObject("flowUnit"))//计量单位
             tasks.add(objTask)//添加任务表
             ct1118.put("tasks", tasks);  //添加任务表
 
             //组装移动应用的周转箱表
             //从当前界面passbox数据复制到指定BmfClass 中,然后将id和submit置空
-            BmfObject passBoxb =  BmfUtils.genericFromJsonExt(passBox, "CT1118PassBoxes");
-            passBoxb.put("id", null);
-            passBoxb.put("submit", false);
+            BmfObject passBoxb =  BmfUtils.genericFromJsonExt(passBox, "CT1118PassBoxes")
+            passBoxb.put("id", null)
+            passBoxb.put("submit", false)
             ct1118.put("passBoxes", Collections.singletonList(passBoxb));//添加周转箱表
 
 
-            sceneGroovyService.buzSceneStart("CT1118",ct1118);
+            sceneGroovyService.buzSceneStart("CT1118",ct1118)
 
             //回入库申请单的状态为:Received-已收货
             BmfObject warehouseInApplication=new BmfObject("WarehouseInApplication")
