37d184a9b6cee28fa1bb8d34f4dd28ee661c7153
yyj <yingjie.yuan@inc.chinajey.com>
2025-01-06 23:27:35 +0800

    1

---
+++ dwork-config-server/src/main/resources/groovy/node_scan_groovy/NodeCT1110Scan.groovy
@@ -0,0 +1,103 @@
+package groovy.node_scan_groovy
+
+import com.alibaba.fastjson.JSONArray
+import com.alibaba.fastjson.JSONObject
+import com.chinajay.virgo.bmf.obj.BmfObject
+import com.chinajay.virgo.utils.BmfUtils
+import com.chinajay.virgo.utils.SpringUtils
+import com.tengnat.dwork.modules.manufacturev2.domain.DomainScanResult
+import com.tengnat.dwork.modules.script.abstracts.NodeScanGroovyClass
+import com.tengnat.dwork.modules.script.service.BasicGroovyService
+import com.tengnat.dwork.modules.script.service.SceneGroovyService
+
+class NodeCT1110Scan extends NodeScanGroovyClass {
+
+    //基础类
+    BasicGroovyService basicGroovyService = SpringUtils.getBean(BasicGroovyService.class)
+    //通用业务类
+    SceneGroovyService sceneGroovyService = SpringUtils.getBean(SceneGroovyService.class)
+
+    @Override
+    JSONObject runScript(BmfObject nodeData) {
+
+        def result = new DomainScanResult()
+        //扫描的组件模块
+        def model = nodeData.getString("model")
+        //扫描的编码
+        def passBoxCode = nodeData.getString("code")
+        //如果扫描模块是物料关联虚拟周转箱，则进行判断。
+        if ("virtualPassBox" == model) {
+            /*
+            virtualPassBox-物料关联虚拟周转箱控件
+             */
+            def suggestedQuantity = BigDecimal.ZERO
+            def quantity = nodeData.getBigDecimal("ext_quantity")?:BigDecimal.ZERO //取得总数量 即PDA界面上的装箱数量
+            def singleBoxQuantity = nodeData.getBigDecimal("ext_single_box_quantity")?:quantity//取得每包数量,每包数量为空时，取总数量，即1包
+            def passBoxList = nodeData.getList("passBoxes")
+            //获得PDA界面上所有周转箱数量之和
+            BigDecimal allocedSum = passBoxList.stream()
+                    .map(passBox -> passBox.getBigDecimal("receiveQuantity") ?: BigDecimal.ZERO)
+                    .reduce(BigDecimal.ZERO, BigDecimal::add)
+            // 计算剩余数量
+            BigDecimal remainingQuantity = quantity - allocedSum?:BigDecimal.ZERO
+
+            // 计算自动填入的数量:首先检查剩余数量是否大于等于包装方案的数量，若买足则按包装方案数量填入，若不满足，再检查是否大于零，否则赋值为零。
+            if (remainingQuantity >= singleBoxQuantity) {
+                suggestedQuantity = singleBoxQuantity
+            } else if (remainingQuantity > BigDecimal.ZERO) {
+                suggestedQuantity = remainingQuantity
+            } else {
+                suggestedQuantity = BigDecimal.ZERO//考虑手动改数量后导致剩余数量为负的情况
+            }
+
+            List<BmfObject> oldPassBoxes = new ArrayList<>()
+
+            passBoxList.forEach { passBox ->
+                {
+                    def objPassBox = BmfUtils.genericFromJsonExt(passBox, "CT1110PassBoxes")
+
+                    //objPassBox.put("receiveQuantity",suggestedQuantity)
+                    oldPassBoxes.add(objPassBox)
+
+                }
+                def tasklist = nodeData.getList("tasks")
+                def material = basicGroovyService.getByCode("material", tasklist.get(0).getString("materialCode"))
+                //new一个JSONObject,然后为新增周转箱字段赋值
+                def objPassBox = basicGroovyService.getByCode("passBox", passBoxCode)
+                def passBoxJson = new JSONObject()
+                passBoxJson.put("virtualPassBox", false)
+                passBoxJson.put("materialCode", material.getString("code"))
+                passBoxJson.put("materialName", material.getString("name"))
+                passBoxJson.put("material", material.getInteger("id"))//物料主数据id
+                passBoxJson.put("quantityUnit", material.getAndRefreshBmfObject("flowUnit"))//流转单位
+                passBoxJson.put("passBox", objPassBox.getInteger("id"))//passBox主数据的id
+                passBoxJson.put("passBoxCode", objPassBox.getString("code"))
+                passBoxJson.put("passBoxName", objPassBox.getString("name"))
+                passBoxJson.put("passBoxClassificationAttribute", objPassBox.getAndRefreshBmfObject("passBoxClassification"))//周转箱类别属性id
+                passBoxJson.put("locationCode", nodeData.getString("locationCode"))
+                passBoxJson.put("locationName", nodeData.getString("locationName"))
+                passBoxJson.put("ownerId", nodeData.getInteger("ownerId"))//创建人id
+                passBoxJson.put("ownerCode", nodeData.getString("ownerCode"))//创建人
+                passBoxJson.put("ownerName", nodeData.getString("ownerName"))//创建人名称
+                passBoxJson.put("equipSourceCode", nodeData.getString("equipCode"))
+                passBoxJson.put("equipSourceType", "PDA")//来源设备
+                passBoxJson.put("ext_batch_number", nodeData.getString("ext_batch_number"))
+
+                passBoxJson.put("submit", true)
+                passBoxJson.put("receiveQuantity", suggestedQuantity)//新增周转箱的数量按计算结果获得
+                passBoxJson.put("quantity", new BigDecimal(0))
+
+                def addPassBox = BmfUtils.genericFromJsonExt(passBoxJson, "CT1110PassBoxes")
+                oldPassBoxes.add(addPassBox)//将新的周转箱增加到原有周转箱list中
+
+                nodeData.remove("passBoxes")//移除周转箱
+                JSONArray oldPassBoxesJson = new JSONArray(oldPassBoxes)
+                nodeData.put("passBoxes", oldPassBoxesJson)//将新的周转箱增加到原有周转箱list中
+
+            }
+            return result.success(nodeData)
+        }
+        //return nodeData
+    }
+
+}
